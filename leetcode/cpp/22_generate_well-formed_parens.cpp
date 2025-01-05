93a039091cc932047a039fa0be98a4aa80fdbf56
DC <DCprivate@github.com>
2025-01-05 09:11:25 -0700

    cleaning stage

--- leetcode/cpp/22_generate_well-formed_parens.cpp
+++ leetcode/cpp/22_generate_well-formed_parens.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 void dfs(int left, int right, int n, string s, vector<string>& vec) {
-    if (left == right) {
+    if (left == right && left + right == n * 2) {
         vec.push_back(s);
         //return;
     }
-    cout << s << endl;
+    //cout << s << endl;
     if (left < n) {
         //cout << "increasing left: " << s << endl;
         dfs(left + 1, right, n , s + "(", vec);
@@ -26,7 +26,6 @@ vector<string> generateParenthesis(int n) {
 }
 
 int main() {
-
     vector<string> answer;
     int n = 3;
     answer = generateParenthesis(n);
@@ -36,3 +35,40 @@ int main() {
     }
 
 }
+
+// fastest
+/*
+         void parenth(int n,int left,int right,vector<string>&ans,string &temp)
+         {
+               if(left+right == 2*n)
+               {
+                     ans.push_back(temp);
+                     return;
+               }
+
+              // left parenth 
+
+               if(left<n)
+               {
+                   temp.push_back('(');
+                   parenth(n,left+1,right,ans,temp);
+                   temp.pop_back();
+               }
+
+               //Right parenth
+               if(right<left)
+               {
+                    temp.push_back(')');
+                    parenth(n,left,right+1,ans,temp);
+                   temp.pop_back();  
+               }
+         }
+    vector<string> generateParenthesis(int n) {
+        
+             vector<string>ans;
+             string temp;
+             parenth(n,0,0,ans,temp);
+             return ans;
+
+    }
+*/
