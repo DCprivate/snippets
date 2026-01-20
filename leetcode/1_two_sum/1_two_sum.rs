09a4b3f905f27990be0c3906f724d1a6e0b0de7f
DC <donut_boy_94@yahoo.com>
2026-01-20 15:35:09 -0700

    problem 1 done in rust, fast but there is a faster way using hashmaps

--- leetcode/1_two_sum/1_two_sum.rs
+++ leetcode/1_two_sum/1_two_sum.rs
@@ -1,21 +1,31 @@
 
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> &'static str {
-    
-    for i in &nums {
-        for j in &nums {
-            if i + j == target {
-                return "Found";
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {   
+    let mut v: Vec<i32> = Vec::new();
+
+    for i in 0..nums.len() {
+        for j in i+1..nums.len() {
+            println!("{} : {}, {:?} : {:?}", i, j, nums[i], nums[j]);
+            if nums[i] + nums[j] == target {
+                v.push(i as i32);
+                v.push(j as i32);
+                return v; 
             }
         }
     }
-    return "Not found";
+    return v;
 }
 
 fn main() {
     let v = vec![2, 7, 11, 15];
-    let t = 9;
+    let v2 = vec![3, 2, 4];
+    let v3 = vec![3, 3];
+    let v4 = vec![2, 5, 5, 11];
+    let v5 = vec![-1,-2,-3,-4,-5];
 
-    println!("{}", two_sum(v, t));
+    println!("{:?}", two_sum(v3, 6));
+
+
+    
 }
 
